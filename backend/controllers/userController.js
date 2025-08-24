832c6856ea74a715f925f7f3a893409074214969
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-24 13:10:37 +0530

    Completed login logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,11 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrypt from "bcryptjs";
-import createToken from "../utils/ceateToken.js"
+import createToken from "../utils/ceateToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
   //Register user function
   const { username, email, password } = req.body;
-
   if (!username || !email || !password) {
     throw new Error("All inputs are requires");
   }
@@ -14,20 +13,19 @@ const createUser = asyncHandler(async (req, res) => {
   const userExists = await User.findOne({ email });
   if (userExists) res.status(400).send("User already exists");
 
-  const salt = await bcrypt.genSalt(7)
-  const hashedPassword = await bcrypt.hash(password, salt)
-  const newUser = new User({ username, email, password:hashedPassword  });
+  const salt = await bcrypt.genSalt(7);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     await newUser.save();
-    createToken(res, newUser._id)
-
-    res.status(201)
-    .json({
+    createToken(res, newUser._id);
+    res.status(201).json({
       _id: newUser._id,
       username: newUser.username,
       email: newUser.email,
       isAdmin: newUser.isAdmin,
+      message:"User register succesfully"
     });
   } catch (error) {
     res.status(400);
@@ -35,4 +33,41 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { createUser };
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+        message:"User login succesfully"
+      });
+      return
+    }
+  }
+});
+
+
+const logoutCurrentUser = asyncHandler(async(req, res)=>{
+   res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0)
+   });
+   res.status(200).json({
+    message: "Logged out successfully"
+   })
+})
+
+export { createUser, loginUser, logoutCurrentUser };
